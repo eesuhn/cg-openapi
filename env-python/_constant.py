20c52c137c5976559936cba8cd2a934624432511
eesuhn <eason.yihong@gmail.com>
2025-03-03 02:18:40 +0800

    Add sample call of test sdk

---
+++ env-python/_constant.py
@@ -0,0 +1,8 @@
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+CG_PRO_API_KEY = os.getenv("CG_PRO_API_KEY")
+CG_DEMO_API_KEY = os.getenv("CG_DEMO_API_KEY")
--- env-python/main.py
+++ env-python/main.py
@@ -1,8 +1,40 @@
 import test_coingecko_python
 
+from test_coingecko_python.rest import ApiException
+from _constant import (
+    CG_DEMO_API_KEY,
+    CG_PRO_API_KEY
+)
+
 
 def main():
-    print("Hello from main.py")
+    configuration = test_coingecko_python.Configuration(
+        host = "https://api.coingecko.com/api/v3"
+    )
+    # configuration.api_key['proKeyAuth'] = CG_PRO_API_KEY
+    configuration.api_key['demoKeyAuth'] = CG_DEMO_API_KEY
+    with test_coingecko_python.ApiClient(configuration) as api_client:
+        # get_networks_list(api_client)
+        get_simple_supported_currencies(api_client)
+        pass
+
+
+def get_networks_list(api_client):
+    api_instance = test_coingecko_python.NetworksApi(api_client)
+    try:
+        api_response = api_instance.networks_list()
+        print(api_response.to_dict())
+    except ApiException as e:
+        print("Exception when calling NetworksApi->networks_list: %s\n" % e)
+
+
+def get_simple_supported_currencies(api_client):
+    api_instance = test_coingecko_python.SimpleApi(api_client)
+    try:
+        api_response = api_instance.simple_supported_currencies()
+        print(api_response)
+    except ApiException as e:
+        print("Exception when calling SimpleApi->simple_supported_currencies: %s\n" % e)
 
 
 if __name__ == "__main__":
