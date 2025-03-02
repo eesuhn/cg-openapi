5c9e5bc71677df5dfe91e5034d6df1024be23312
eesuhn <eason.yihong@gmail.com>
2025-03-03 00:47:11 +0800

    Force-clean target folder before openapi-generate

--- openapi.sh
+++ openapi.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(dirname $(readlink -f $0))
+TARGET_DIR="$SCRIPT_DIR/test-coingecko-python"
+if [ -d "$TARGET_DIR" ]; then
+    find $TARGET_DIR -mindepth 1 -not -name '.gitkeep' -exec rm -rf {} +
+fi
 openapi-generator generate \
     -i $SCRIPT_DIR/coingecko-api.yml \
     -g python \
-    -o $SCRIPT_DIR/test-coingecko-python \
+    -o $TARGET_DIR \
     -c $SCRIPT_DIR/openapi-config.json
